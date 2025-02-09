64008d758faa65bd7b00293588e8617dfd0ef92b
Amit  Biswas <amitbiswas1992@github.com>
2025-02-09 18:36:25 -0500

    Improve deployment automation with logs and cleanup- 04

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">Please feel free to add an item!</p>
+                <p className="text-center">Please feel free to add an item! Thanks!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
